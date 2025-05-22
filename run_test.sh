bd9582768a5aaa5aded9c7e075df9c71164f5589
Totobird <45902300+Totobird-Creations@users.noreply.github.com>
2025-05-22 13:07:21 -0400

    Reformatted run_test.sh;

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,21 @@
+# Use this example from flywheel-sys-rs
 EXAMPLE_NAME=big_clock
 
-reset
-cargo build &&
-(cd ../flywheel-sys-rust/examples/${EXAMPLE_NAME}; ./build.sh) &&
-./target/debug/flywheelmc -b=127.0.0.1:25580 -l=trace,flywheelmc_players::conn=debug,flywheelmc_players::world=debug,flywheelmc_wasm::runner::event=debug ../flywheel-sys-rust/examples/${EXAMPLE_NAME}/target/wasm32-unknown-unknown/release/${EXAMPLE_NAME}.wasm
+# Clear Terminal
+clear
+
+# Build flywheelmc-server-selfhosted
+cargo build || exit 1
+
+# Build the selected example script
+(cd ../flywheel-sys-rust/examples/${EXAMPLE_NAME}; ./build.sh) || exit 1
+
+# Start the server
+./target/debug/flywheelmc \
+  -b=127.0.0.1:25565 \
+  -l="trace,\
+       flywheelmc_players::conn=debug,\
+       flywheelmc_players::world=debug,\
+       flywheelmc_wasm::runner::event=debug" \
+  ../flywheel-sys-rust/examples/${EXAMPLE_NAME}/target/wasm32-unknown-unknown/release/${EXAMPLE_NAME}.wasm
+
